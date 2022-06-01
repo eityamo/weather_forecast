26e2dcd3483bb61b5caed17602be00ac63e14f5f
eityamo <eityamo@gmail.com>
2022-06-01 12:10:32 +0900

    tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,15 @@
 class TweetsController < ApplicationController
+  before_action :twitter_client, except: :new
+
+  def twitter_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = 'PgzAlGqrfIQPwsoIrfZYRRYZq'
+      config.consumer_secret = 'gQyeNt9In4jIOj50Uh3nSLqIfCU8NcZPGMI0246ayMqvU5RqHm'
+      config.access_token = '1531062177678827520-TKJvIzVjeEa9OCs1NJ3RyqW3K5qspX'
+      config.access_token_secret = 'ehsEvQIJDds92TxBNbq748T0XEI7vtPnVJbztyHigO2H4'
+    end
+  end
+
   def new
     @tweet = Tweet.new
   end
@@ -6,6 +17,11 @@ class TweetsController < ApplicationController
   def create
     Tweet.create(create_params)
     redirect_to :root
+  end
+
+  def post
+    status = '今日は晴れ'
+    @client.update(status)
   end
 
   private
