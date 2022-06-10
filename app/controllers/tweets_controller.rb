c9c73ff356719344ae858c8c23aa91c9f3463e7b
eityamo <eityamo@gmail.com>
2022-06-10 10:11:26 +0900

    ランダムでFANZA女優が出てくる

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,9 @@
 require 'net/http'
 require 'uri'
+require 'open-uri'
+# Don't allow downloaded files to be created as StringIO. Force a tempfile to be created.
+OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
+OpenURI::Buffer.const_set 'StringMax', 0
 
 class TweetsController < ApplicationController
   before_action :twitter_client, only: %i[post]
@@ -17,6 +21,7 @@ class TweetsController < ApplicationController
 
   def post
     prefecture = Prefecture.all.sample
+    actress = Actress.where(prefecture_id: prefecture.id).sample
     uri = URI.parse("https://weather.tsukumijima.net/api/forecast/city/#{prefecture.forecast}")
     json = Net::HTTP.get(uri)
     result = JSON.parse(json, { symbolize_names: true })
@@ -24,8 +29,10 @@ class TweetsController < ApplicationController
     telop = result[:forecasts][1][:telop]
     max_celsius = result[:forecasts][1][:temperature][:max][:celsius]
     min_celsius = result[:forecasts][1][:temperature][:min][:celsius]
-    status = "明日の#{prefecture}の天気は#{telop}、最高気温は#{max_celsius}℃、最低気温は#{min_celsius}℃です。"
-    @client.update(status)
+    status = "お疲れ様です、明日のお天気をお伝えいたします。\n明日の#{prefecture}の天気は#{telop}、最高気温は#{max_celsius}℃、最低気温は#{min_celsius}℃です。\n今夜のお天気お姉さん:#{actress.name}(#{actress.cup})\n#{actress.digital}\n#今夜のお天気お姉さん\n#{actress.image}"
+    image_uri = URI.parse(actress.image)
+    media = image_uri.open
+    @client.update_with_media(status, media)
     redirect_to :root
   end
 end
